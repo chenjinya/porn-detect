4fe3973527ac27b0a64aaba40c33645214bf70ea
Jinya Chen <jinyachen@gmail.com>
2019-08-22 18:10:00 +0800

    first commit

---
+++ porn_detect.py
@@ -0,0 +1,85 @@
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os,stat
+import urllib.request 
+import sys,PIL.Image as Image
+import string
+import numpy
+import time;
+
+current_path = os.getcwd();
+
+log_path = current_path + '/porn.log-' + time.strftime("%Y-%m-%d", time.localtime());
+log_file_cache = open(log_path, 'a+');
+
+def flattenList(l):
+    if False == isinstance(l,list):
+        print(l, 'is not `list`');
+        return False;
+
+    _list = [];
+    for item in l :
+        if isinstance(item,list):
+            _list.extend(flattenList(item))
+        else:
+            _list.append(item)
+    return _list;
+
+def log(*content):
+    datetime = time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime());
+    log_file_cache.writelines(flattenList([datetime,list(content), "\n"]));
+
+
+
+def downloadFile(remote_path, dir_path):
+    try:
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        arr = os.path.split(remote_path);
+        file_name = arr[1];
+
+        file_path = '{}{}{}'.format(dir_path,os.sep,file_name)
+        log("saving to: " + file_path)
+        urllib.request.urlretrieve(remote_path,file_path)
+        return file_path;
+    except IOError as e:
+        log("IOError")
+    except Exception as e:
+        log(e, sys._getframe().f_lineno)
+
+def pornDetect(image_path):
+    img = Image.open(image_path).convert('YCbCr')
+    w, h = img.size
+    data = img.getdata()
+    cnt = 0
+    for i, ycbcr in enumerate(data):
+        y, cb, cr = ycbcr
+        if 86 <= cb <= 117 and 140 <= cr <= 168:
+            cnt += 1
+    rate = cnt / ((w * h * 0.1))
+
+    return rate
+
+
+params1 = sys.argv[1];
+log("argv: ", sys.argv)
+
+current_path = os.getcwd();
+log("current path: %s"%(current_path))
+
+
+dir_path='%s/porn_detect_temp'%(current_path)
+
+file_path = downloadFile(params1, dir_path)
+
+log("download file path: %s"%(file_path))
+
+score = pornDetect(file_path)
+
+log_file_cache.close();
+os.remove(file_path);
+print("porn detect: %s %s %s"%(params1,'socre: ' + str(score),  ' is true' if score > 1 else 'is false'))
+
